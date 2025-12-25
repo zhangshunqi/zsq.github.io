f21a7448ecc68409564f8417dbe5fdb86002a245
Zhang Shunqi <zsq180255@qq.com>
2025-12-25 14:59:40 +0800

    add line count function

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 
 int main() {
     parse("Hello Git & GCC!");
-    // 分支 feature/linecount 中添加行统计测试
-    // int lines = count_lines("main.c");
-    // printf("Line count of main.c: %d\n", lines);
+    int lines = count_lines("main.c");
+    printf("Line count of main.c: %d\n", lines);
     return 0;
 }
